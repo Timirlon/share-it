e88e61abdaa7b2696b88ce6742293cbe0eb23d9a
Timirlon <tturgimbayev@gmail.com>
2025-02-23 12:36:24 +0500

    Revert "share-it v.1.1.1 - minor fixes in code"
    This reverts commit bc323b9c39048507e95cc2af01b71b832d3343da.

--- src/main/java/org/example/shareit/user/UserDto.java
+++ src/main/java/org/example/shareit/user/UserDto.java
@@ -1,7 +1,5 @@
 package org.example.shareit.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,12 +10,6 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
     int id;
-
-    @NotBlank
     String name;
-
-    @Email
     String email;
-
-    //Добавить новый класс (мб)
 }
--- src/main/java/org/example/shareit/utils/RequestConstant.java
+++
@@ -1,5 +0,0 @@
-package org.example.shareit.utils;
-
-public class RequestConstant {
-    public static final String USER_HEADER = "X-Sharer-User-Id";
-}

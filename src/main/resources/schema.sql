d454f0e6e9887f21c5189abe0636e78ba38b52da
Timirlon <tturgimbayev@gmail.com>
2025-02-28 14:58:38 +0500

    Minor update in db schema file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
                        id SERIAL PRIMARY KEY,
                        name VARCHAR(255),
                        email VARCHAR(255) UNIQUE
 );
 
-CREATE TABLE items (
+CREATE TABLE IF NOT EXISTS items (
                        id SERIAL PRIMARY KEY,
                        name VARCHAR(255),
                        description VARCHAR(255),
@@ -12,7 +12,7 @@ CREATE TABLE items (
                        owner_id INT REFERENCES users
 );
 
-CREATE TABLE bookings (
+CREATE TABLE IF NOT EXISTS bookings (
                           id SERIAL PRIMARY KEY,
                           start_date TIMESTAMP WITHOUT TIME ZONE,
                           end_date TIMESTAMP WITHOUT TIME ZONE,
@@ -21,7 +21,7 @@ CREATE TABLE bookings (
                           status VARCHAR(255)
 );
 
-CREATE TABLE comments (
+CREATE TABLE IF NOT EXISTS comments (
                           id SERIAL PRIMARY KEY,
                           text VARCHAR(255),
                           item_id INT REFERENCES items,

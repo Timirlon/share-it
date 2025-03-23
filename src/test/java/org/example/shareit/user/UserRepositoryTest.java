4ee312479760adbb1abf987c33abd0567583f8bb
Timirlon <tturgimbayev@gmail.com>
2025-03-23 11:34:34 +0500

    Share It v.3.2.1 - upd UserRepositoryTest

--- src/test/java/org/example/shareit/user/UserRepositoryTest.java
+++ src/test/java/org/example/shareit/user/UserRepositoryTest.java
@@ -1,6 +1,9 @@
 package org.example.shareit.user;
 
 import jakarta.persistence.EntityManager;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -12,7 +15,23 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Test
     void saveTest() {
         User user = new User();
+
+        String userName = "Человек";
+        String userEmail = "chel123@mail.com";
+        user.setName(userName);
+        user.setEmail(userEmail);
+        userRepository.save(user);
+
+
+        entityManager.clear();
+
+        User foundUser = userRepository.findById(user.getId()).orElseThrow();
+
+
+        assertEquals(userName, foundUser.getName());
+        assertEquals(userEmail, foundUser.getEmail());
     }
 }

a87cb38d1020de4cf543e9317fffa2e68a4cce5d
Timirlon <tturgimbayev@gmail.com>
2025-03-23 11:51:43 +0500

    Share It v.3.2.2 - upd UserServiceTest + pom.xml (added h2 dep.)

--- src/test/java/org/example/shareit/user/UserServiceTest.java
+++ src/test/java/org/example/shareit/user/UserServiceTest.java
@@ -1,4 +1,43 @@
 package org.example.shareit.user;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Spy
+    UserMapper userMapper;
+
+    @Test
+    void findByIdTest() {
+        User user = new User();
+        user.setId(1);
+        user.setName("Вася");
+        user.setEmail("VasilyTyomny@mail.com");
+
+
+        Mockito.when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+
+
+        UserDto foundUser = userService.findById(user.getId());
+
+        assertEquals(user.getId(), foundUser.getId());
+        assertEquals(user.getName(), foundUser.getName());
+        assertEquals(user.getEmail(), foundUser.getEmail());
+    }
 }

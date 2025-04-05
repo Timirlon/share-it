2c17abc0d4ad481ea30ceb928cd8732a6558d7fd
Timirlon <tturgimbayev@gmail.com>
2025-04-06 01:07:14 +0500

    Share It v.3.2.28 - bookings test upd (100% coverage of bookings package)

--- src/test/java/org/example/shareit/bookings/BookingControllerTest.java
+++ src/test/java/org/example/shareit/bookings/BookingControllerTest.java
@@ -17,13 +17,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.example.shareit.utils.RequestConstants.USER_ID_REQUEST_HEADER;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @WebMvcTest({BookingController.class, BookingMapper.class,
         ItemMapper.class, UserMapper.class, CommentMapper.class})
@@ -41,7 +41,7 @@ public class BookingControllerTest {
 
     @Test
     @SneakyThrows
-    void createTestSuccess() {
+    void createTest() {
         int bookerId = 1;
         String bookerName = "booker-test";
         String bookerEmail = "booker-test@mail.com";
@@ -101,5 +101,180 @@ public class BookingControllerTest {
                 .andExpect(jsonPath("$.status").value(BookingStatus.WAITING.toString()));
     }
 
+    @Test
+    @SneakyThrows
+    void updateStatusTest() {
+        boolean isApproved = true;
 
+        User booker = new User();
+        int bookerId = 1;
+        booker.setId(bookerId);
+        booker.setName("test-booker-name");
+
+        Item item = new Item();
+        int itemId = 1;
+        item.setId(itemId);
+        User itemOwner = new User();
+        item.setOwner(itemOwner);
+
+        Booking booking = new Booking();
+        int bookingId = 1;
+        booking.setId(bookingId);
+        booking.setStartDate(LocalDateTime.now());
+        booking.setEndDate(LocalDateTime.now().plusHours(5));
+        booking.setStatus(BookingStatus.APPROVED);
+        booking.setBooker(booker);
+        booking.setItem(item);
+
+
+        Mockito.when(bookingService.updateStatus(bookingId, bookerId, isApproved))
+                .thenReturn(booking);
+
+
+        mockMvc.perform(patch("/bookings/" + bookingId
+                + "?approved=" + isApproved)
+                    .header(USER_ID_REQUEST_HEADER, bookerId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(bookingId))
+                .andExpect(jsonPath("$.start").exists())
+                .andExpect(jsonPath("$.end").exists())
+                .andExpect(jsonPath("$.item.id").value(itemId))
+                .andExpect(jsonPath("$.booker.id").value(bookerId))
+                .andExpect(jsonPath("$.status").value(BookingStatus.APPROVED.toString()));
+    }
+
+    @Test
+    @SneakyThrows
+    void findByIdTest() {
+        User booker = new User();
+        int bookerId = 1;
+        booker.setId(bookerId);
+        booker.setName("test-booker-name");
+
+        Item item = new Item();
+        int itemId = 1;
+        item.setId(itemId);
+        User itemOwner = new User();
+        item.setOwner(itemOwner);
+
+        Booking booking = new Booking();
+        int bookingId = 1;
+        booking.setId(bookingId);
+        booking.setStartDate(LocalDateTime.now());
+        booking.setEndDate(LocalDateTime.now().plusHours(5));
+        booking.setStatus(BookingStatus.APPROVED);
+        booking.setBooker(booker);
+        booking.setItem(item);
+
+
+        Mockito.when(bookingService.findById(bookingId, bookerId))
+                .thenReturn(booking);
+
+
+        mockMvc.perform(get("/bookings/" + bookingId)
+                        .header(USER_ID_REQUEST_HEADER, bookerId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(bookingId))
+                .andExpect(jsonPath("$.start").exists())
+                .andExpect(jsonPath("$.end").exists())
+                .andExpect(jsonPath("$.item.id").value(itemId))
+                .andExpect(jsonPath("$.booker.id").value(bookerId))
+                .andExpect(jsonPath("$.status").value(BookingStatus.APPROVED.toString()));
+    }
+
+    @Test
+    @SneakyThrows
+    void findMyBookingTest() {
+        int expectedSize = 1;
+
+        User booker = new User();
+        int bookerId = 1;
+        booker.setId(bookerId);
+        booker.setName("test-booker-name");
+
+        Item item = new Item();
+        int itemId = 1;
+        item.setId(itemId);
+        User itemOwner = new User();
+        item.setOwner(itemOwner);
+
+        Booking booking = new Booking();
+        int bookingId = 1;
+        booking.setId(bookingId);
+        booking.setStartDate(LocalDateTime.now());
+        booking.setEndDate(LocalDateTime.now().plusHours(5));
+        booking.setStatus(BookingStatus.APPROVED);
+        booking.setBooker(booker);
+        booking.setItem(item);
+
+        BookingFilteringState defaultState = BookingFilteringState.ALL;
+        int defaultPage = 0;
+        int defaultSize = 10;
+
+        Mockito.when(bookingService.findAllByBookerId(
+                bookerId, defaultState, defaultPage, defaultSize))
+                .thenReturn(List.of(booking));
+
+
+        mockMvc.perform(get("/bookings")
+                        .header(USER_ID_REQUEST_HEADER, bookerId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(expectedSize))
+                .andExpect(jsonPath("$.[0].id").value(bookingId))
+                .andExpect(jsonPath("$.[0].start").exists())
+                .andExpect(jsonPath("$.[0].end").exists())
+                .andExpect(jsonPath("$.[0].item.id").value(itemId))
+                .andExpect(jsonPath("$.[0].booker.id").value(bookerId))
+                .andExpect(jsonPath("$.[0].status").value(BookingStatus.APPROVED.toString()));
+    }
+
+    @Test
+    @SneakyThrows
+    void findBookingOfMyItemTest() {
+        int expectedSize = 1;
+
+        User user = new User();
+        int bookerId = 1;
+        user.setId(bookerId);
+        user.setName("test-user-name-1");
+
+        User itemOwner = new User();
+        int ownerId = 2;
+        itemOwner.setId(ownerId);
+        itemOwner.setName("test-user-name-2");
+
+        Item item = new Item();
+        int itemId = 1;
+        item.setId(itemId);
+        item.setOwner(itemOwner);
+
+        Booking booking = new Booking();
+        int bookingId = 1;
+        booking.setId(bookingId);
+        booking.setStartDate(LocalDateTime.now());
+        booking.setEndDate(LocalDateTime.now().plusHours(5));
+        booking.setStatus(BookingStatus.APPROVED);
+        booking.setBooker(user);
+        booking.setItem(item);
+
+        BookingFilteringState defaultState = BookingFilteringState.ALL;
+        int defaultPage = 0;
+        int defaultSize = 10;
+
+        Mockito.when(bookingService.findAllByItemOwnerId(
+                        ownerId, defaultState, defaultPage, defaultSize))
+                .thenReturn(List.of(booking));
+
+
+        mockMvc.perform(get("/bookings/owner")
+                        .header(USER_ID_REQUEST_HEADER, ownerId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(expectedSize))
+                .andExpect(jsonPath("$.[0].id").value(bookingId))
+                .andExpect(jsonPath("$.[0].start").exists())
+                .andExpect(jsonPath("$.[0].end").exists())
+                .andExpect(jsonPath("$.[0].item.id").value(itemId))
+                .andExpect(jsonPath("$.[0].booker.id").value(bookerId))
+                .andExpect(jsonPath("$.[0].status").value(BookingStatus.APPROVED.toString()));
+    }
 }
--- src/test/java/org/example/shareit/bookings/BookingServiceIT.java
+++
@@ -1,4 +0,0 @@
-package org.example.shareit.bookings;
-
-public class BookingServiceIT {
-}

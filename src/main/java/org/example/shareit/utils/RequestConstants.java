6d1e31defb57a03c5356bd965512b5bcfdff7deb
Timirlon <tturgimbayev@gmail.com>
2025-04-08 17:18:56 +0500

    Share It v.3.3 - minor upd pom.xml and RequestConstants.java (for jacoco)

--- src/main/java/org/example/shareit/utils/RequestConstants.java
+++ src/main/java/org/example/shareit/utils/RequestConstants.java
@@ -1,5 +1,8 @@
 package org.example.shareit.utils;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class RequestConstants {
     public static final String USER_ID_REQUEST_HEADER = "X-Sharer-User-Id";
 }
